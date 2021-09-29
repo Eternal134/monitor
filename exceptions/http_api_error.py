2a97cdd25479049a1ad17b84ea9896579db1e0b0
Eternal134 <1298749337@qq.com>
2021-09-29 14:22:03 +0800

    初步实现测试接口

---
+++ exceptions/http_api_error.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+# author: qiumao
+# datetime:2021/9/29 13:29
+
+class BaseHttpApiError(BaseException):
+
+    def __init__(self, url, message):
+        # 测试的http接口
+        self.url = ""
+        # 描述信息
+        self.message = ""
+
+
+class HttpApiIncorrectDataError(BaseHttpApiError):
+    """ HTTP接口返回数据不正确
+    """
+
+    def __init__(self, url: str, exceptData: str, realData: str, message: str = ""):
+        """
+        @param url: HTTP地址
+        @param exceptData: 期望的数据
+        @param realData: 实际的数据
+        @param message: 描述信息
+        @return: HttpApiIncorrectDataError
+        """
+        presetMessage = f"地址 = {url} 返回的数据不正确。\n 期望的数据为: {exceptData} \n 实际的数据为: {realData}"
+        _message = message if message != "" else presetMessage
+        super().__init__(url, _message)
+
+
+class HttpApiIncorrectStatusCodeError(BaseHttpApiError):
+    """ HTTP接口状态码不正确 """
+
+    def __init__(self, exceptStatusCode: int, url: str, message: str = ""):
+
+        presetMessage = f"地址 = {url} 返回的状态码不是 {exceptStatusCode} ！"
+        _message = message if message != "" else presetMessage
+        super().__init__(url, _message)
+
+
+class HttpApiNot200ErrorError(HttpApiIncorrectStatusCodeError):
+    """ HTTP接口状态码非200错误码
+    """
+
+    def __init__(self, url, message=""):
+
+        super().__init__(200, url, message)
--- test.py
+++ test.py
@@ -3,26 +3,73 @@ from datetime import datetime
 
 import requests
 
-def httpApiTestTemplate(url, method, expectedStatusCode=200, expectedResponseData=None, postForm=None):
-    """ HTTP接口测试模板 """
-    
+from exceptions.http_api_error import HttpApiIncorrectStatusCodeError, HttpApiIncorrectDataError, BaseHttpApiError
+
+
+def httpApiTestTemplate(url: str, method: str, headers: dict = None,
+                        expectedStatusCode: int = 200, expectedResponseData: str = None,
+                        postData: dict = None) -> int:
+    """
+    Http接口测试模板
+    @param url: http api接口链接地址
+    @param method: 测试方法，支持[GET, POST]
+    @param headers: 请求头
+    @param expectedStatusCode: 预期的HTTP状态码。默认为200
+    @param expectedResponseData: 预期的返回值。默认为空
+    @param postData: 使用POST方法时，提交的数据。默认为空
+    @return: 测试通过，返回0；不通过，抛出异常
+    """
+
+    if method == 'GET':
+        response = requests.get(url, headers=headers)
+    elif method == 'POST':
+        response = requests.post(url, headers=headers, data=postData)
+    else:
+        raise Exception("method方法选择错误")
+
+    if response.status_code != expectedStatusCode:
+        raise HttpApiIncorrectStatusCodeError(expectedStatusCode, url)
+
+    realData = response.text
+    if expectedResponseData is not None and realData != expectedResponseData:
+        raise HttpApiIncorrectDataError(url, expectedResponseData, realData)
+
+    return 0
+
+
+def httpGetApiTest200StatusCode(url: str):
+    """
+    使用http get方法测试api是否返回200状态码
+    @param url: 地址
+    @return: 测试通过，返回0；不通过，抛出异常
+    """
+
+    httpApiTestTemplate(url, method='GET')
+
 
 def ahuBackEndApiMonitor():
     """ 安大通后端接口监控 """
-    
+
     # 后端域名
     backEndDomain = "https://ahuer.cn/"
     # 测试接口地址
     helloApiUrl = "api/"
-    
-    
-    print("现在是: ", datetime.now())
-    
-if __name__ == '__main__':
-    
-    scheduler = BlockingScheduler()
-    
+
     try:
+        url = backEndDomain+helloApiUrl
+        httpGetApiTest200StatusCode(url)
+        print(f"接口测试通过, url=[{url}], [{datetime.now()}]")
+    except BaseHttpApiError as error:
+        print(f"接口测试出现异常, message={error.message}, [{datetime.now()}]")
+
+
+if __name__ == '__main__':
+
+    scheduler = BlockingScheduler()
+
+    try:
+        scheduler.add_job(ahuBackEndApiMonitor, 'interval', seconds=3)
         scheduler.start()
+        print("***** 定时调度任务启动 ***** ")
     except (SystemExit, KeyboardInterrupt):
         print("application已被强制结束")
